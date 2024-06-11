0b76cfd88b6017d29364dcfeb2b29711971f85fe
MizaelSiebenSilvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-06-11 18:13:14 -0300

    update calculadora

--- script.js
+++ script.js
@@ -13,20 +13,6 @@ document.addEventListener("keypress", function(event) {
         console.log(event.key)
 })
 
-// const calc = [ 'Number', '/', '*', '-', '+' ]
-
-// const res = calc.includes( 'Number', '/', '*', '-', '+' )
-
-//     if (res) {
-//         console.log("É uma tecla da calculadora" )
-//     } else {
-//         console.log("Não é uma tecla da calculadora!")
-//     }
-
-
-
-
-
 let teclas = [ "Number", "/", "*", "-", "+", "ENTER", "." ]
 
 console.log( teclas.includes( "event.key" ) )
@@ -36,11 +22,3 @@ console.log( teclas.includes( "event.key" ) )
     } else {
         console.log('Não é uma tecla da calculadora')
     }
-
-// const fruits = ['Maçã', 'Manga']
-// const res = fruits.includes('Banana')
-// if (res) {
-//     console.log('Ebaaa!')
-// } else {
-//     console.log(`Vamos comprar uma laranja`)
-// }
