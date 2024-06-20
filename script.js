30b4556bf9fde953286acd84291e1bc16b85d6f8
Mizael Sieben Silvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-06-20 18:09:14 -0300

    update Calculadora

--- script.js
+++ script.js
@@ -2,22 +2,20 @@ function formatarNumero(numero) {
     if (isNaN(numero)) {
         return ''
     }
-    numero = parseInt(numero)
-    return numero.toLocaleString({
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 10
+    numero = parseFloat(numero)
+    return numero.toLocaleString('pt-BR',{
+        maximumFractionDigits: 2
     })
 }
 const validarEntrada = entrada => {
-    let teclasPermitidas = ["+", "-", "*", "X", "+", "C", "+/-", "%", "÷", ".", "0"]
+    let teclasPermitidas = ["+", "-", "*", "X", "C", "+/-", "%", "÷", ".", "0"]
     return (+entrada > 0 || teclasPermitidas.includes(entrada))
 }
 let buttons = document.querySelectorAll('button')
 let display = document.querySelector('.resultado')
 
 const atualizaDisplay = tecla => {
-    if (tecla === "C") {
-        display.innerHTML = ''
+    if (tecla === "C") { display.innerHTML = ''
         return
     }
     if (tecla === '.' && display.innerHTML.indexOf(".") >= 0) {
