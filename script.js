7e2dc90fa53e2cc33d28cacb62e33b84b90f4a73
Mizael Sieben Silvano <96353670+mizaelsiebensilvano@users.noreply.github.com>
2024-06-20 17:44:28 -0300

    update Calculadora

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
+function formatarNumero(numero) {
+    if (isNaN(numero)) {
+        return ''
+    }
+    numero = parseInt(numero)
+    return numero.toLocaleString({
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 10
+    })
+}
+const validarEntrada = entrada => {
+    let teclasPermitidas = ["+", "-", "*", "X", "+", "C", "+/-", "%", "÷", ".", "0"]
+    return (+entrada > 0 || teclasPermitidas.includes(entrada))
+}
 let buttons = document.querySelectorAll('button')
+let display = document.querySelector('.resultado')
 
+const atualizaDisplay = tecla => {
+    if (tecla === "C") {
+        display.innerHTML = ''
+        return
+    }
+    if (tecla === '.' && display.innerHTML.indexOf(".") >= 0) {
+        return
+    }
+    if (tecla === '.' && display.innerHTML === "") {
+        display.innerHTML = '0'+ tecla
+        return
+    }
+    if (+tecla > 0 || tecla === "0" || tecla === ".")
+        display.innerHTML = +''+ tecla
+}
 buttons.forEach(function(button){
-
-    
     button.addEventListener('click', function(e) {
-        console.log(e.target.textContent)
+        let tecla = e.target.textContent
+        if (validarEntrada(tecla)) {
+            atualizaDisplay(tecla)
+        }
     })
 })
-
-document.addEventListener("keypress", function(event) {
-
-        console.log(event.key)
-})
-
-let teclas = [ "Number", "/", "*", "-", "+", "ENTER", "." ]
-
-console.log( teclas.includes( "event.key" ) )
-
-    if (true.event.key) {
-        console.log('É uma tecla da calculadora')
-    } else {
-        console.log('Não é uma tecla da calculadora')
+document.addEventListener('keypress', function(e){
+    let tecla = e.key;
+    if (validarEntrada(tecla)) {
+        atualizaDisplay(tecla);
     }
+})
--- texte.js
+++ texte.js
@@ -1,1 +1,33 @@
-console.log('teste')
+function formatarNumero(numero) {
+    if(isNaN(numero)) {
+        return ''
+    }
+    numero = parseFloat(numero)
+    return numero.toLocaleString('pt-BR', {
+        maximumFractionDigits: 2
+    })
+}
+
+const validarEntrada = entrada => {
+    let teclasPermitidas = [ "÷", "*", "-", "+", "%", "+/-", ".", "[0]" ]
+    return (+entrada > 0 || teclasPermitidas.includes(entrada))
+}
+
+let buttons = document.querySelectorAll('button')
+let display = document.querySelector('.resultado')
+
+const atualizaDisplay = tecla => {
+    if(tecla === "C") {
+        display.innerHTML = ''
+        return
+    }
+    if(tecla === "." && display.innerHTML.indexOf(".") >= 0) {
+        return
+    }
+    if(tecla === '.' && display.innerHTML === "") {
+        display.innerHTML = '0'+ tecla
+        return
+    }
+    if(+tecla > 0 || tecla === "0" || tecla === ".")
+        display.innerHTML = display.innerHTML +''+ tecla
+}
